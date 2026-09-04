ac5c456288137bffa7a4d928462ce3e27c585693
Avinash Kumar <ksavinash0101@gmail.com>
2026-09-04 17:29:07 +0530

    list base

--- python_basics.py
+++ python_basics.py
@@ -36,3 +36,31 @@ def students(records):
     )
 
     return names
+
+#List
+def process_commands(commands):
+    my_list = []
+
+    for command in commands:
+        command = command.split()
+
+        if command[0] == "insert":
+            my_list.insert(int(command[1]), int(command[2]))
+
+        elif command[0] == "print":
+            print(my_list)
+
+        elif command[0] == "remove":
+            my_list.remove(int(command[1]))
+
+        elif command[0] == "append":
+            my_list.append(int(command[1]))
+
+        elif command[0] == "sort":
+            my_list.sort()
+
+        elif command[0] == "pop":
+            my_list.pop()
+
+        elif command[0] == "reverse":
+            my_list.reverse()
